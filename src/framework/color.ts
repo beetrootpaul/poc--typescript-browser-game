07da2a41d57f72ecf1a6853e4347246049734c3b
Beetroot Paul <paul@beetrootpaul.com>
2023-05-02 22:05:39 +0200

    setup first tests

--- src/framework/color.ts
+++ src/framework/color.ts
@@ -25,3 +25,35 @@ export class Color {
     );
   }
 }
+
+if (import.meta.vitest) {
+  const { describe, test, expect } = import.meta.vitest;
+
+  describe("constructor", () => {
+    test("successful construction", () => {
+      const color = new Color(1, 2, 3);
+
+      expect(color.r).toEqual(1);
+      expect(color.g).toEqual(2);
+      expect(color.b).toEqual(3);
+    });
+
+    test("validation", () => {
+      expect(() => new Color(0, -1, 0)).toThrow(
+        "One of color components is outside 0-255 range"
+      );
+      expect(() => new Color(0, 256, 0)).toThrow(
+        "One of color components is outside 0-255 range"
+      );
+    });
+  });
+
+  describe("#asCssHex", () => {
+    test("successful conversions", () => {
+      expect(new Color(0, 0, 0).asCssHex()).toEqual("#000000");
+      expect(new Color(1, 2, 3).asCssHex()).toEqual("#010203");
+      expect(new Color(241, 242, 243).asCssHex()).toEqual("#f1f2f3");
+      expect(new Color(255, 255, 255).asCssHex()).toEqual("#ffffff");
+    });
+  });
+}
