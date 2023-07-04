64f54261f51240aacd0bbb193c47a3b83637cd1d
Beetroot Paul <paul@beetrootpaul.com>
2023-07-04 23:38:54 +0200

    adjust desired FPS to achieve a real 30 FPS

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,8 @@
 import { xy_ } from "@framework";
 
 export const g = {
-  fps: 30,
+  // TODO: set it back to 30 and re-implement the game loop in a way that its adjusts to really do ~30 FPS
+  fps: 40, // this value results with 30 FPS on my machine
   screenSize: xy_(128, 128),
   cameraOffset: xy_(0, -16),
   musicBeatFrames: 16,
