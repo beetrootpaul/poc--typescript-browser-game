02093155862aa089a63772d2764d5ec5c48f9fd7
Beetroot Paul <paul@beetrootpaul.com>
2023-07-05 00:34:09 +0200

    no longer adjust the desired FPS since it is now done dynamically by the framework

--- src/globals.ts
+++ src/globals.ts
@@ -1,8 +1,7 @@
 import { xy_ } from "@framework";
 
 export const g = {
-  // TODO: set it back to 30 and re-implement the game loop in a way that its adjusts to really do ~30 FPS
-  fps: 40, // this value results with 30 FPS on my machine
+  fps: 30,
   screenSize: xy_(128, 128),
   cameraOffset: xy_(0, -16),
   musicBeatFrames: 16,
