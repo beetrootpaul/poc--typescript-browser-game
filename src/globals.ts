277c0f7e9108cb12f822ba2cfb17fcbacedce471
Beetroot Paul <paul@beetrootpaul.com>
2023-07-06 21:14:47 +0200

    fix topbar size

--- src/globals.ts
+++ src/globals.ts
@@ -5,6 +5,6 @@ export const g = {
   screenSize: xy_(128, 128),
   cameraOffset: xy_(0, -16),
   gameAreaSize: xy_(128, 112),
-  topbarSize: xy_(123, 16),
+  topbarSize: xy_(128, 16),
   musicBeatFrames: 16,
 };
